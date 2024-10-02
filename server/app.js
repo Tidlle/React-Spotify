273e5d5fe104dfac2e3aae7ddf290e537468af56
Tidlle <98828582+Tidlle@users.noreply.github.com>
2024-10-02 08:51:50 -0300

    conexao API

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import conectaNaDb from './db.js';
 import cors from 'cors';
+import artista from './Models/Artista.js';
 
 const app = express();
 app.use(cors());
@@ -14,6 +15,12 @@ conexao.once('open', () => {
     console.log('Conectado ao MongoDB')
 })
 
+app.get('/artistas', async(req,res) => {
+    const listaArtistas = await artista.find({});
+    res.status(200).json(listaArtistas);
+})
+
 app.listen(3000, () => {
     console.log('Servidor conectado')
 })
+
