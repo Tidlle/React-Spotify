1e00197933fee2f698c7823e0a34fba9158b3ea6
Tidlle <98828582+Tidlle@users.noreply.github.com>
2024-10-09 09:31:40 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import CardSideBar from "./components/CardSidebar"
 import Container from "./components/Container"
 import ConteudoPrincipal from "./components/ConteudoPrincipal"
@@ -6,19 +7,41 @@ import SideBar from "./components/SideBar"
 
 function App() {
 
+  const [artistas, setArtistas] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/artistas')
+      .then(res => res.json)
+      .then(data => setArtistas(data))
+      .catch(err => console.log(err))
+  })
+
   return (
     <>
-      <Header/>
+      <Header />
       <Container>
         <SideBar>
-        <CardSideBar/>
-        <CardSideBar/>
-        <CardSideBar/>
-        <CardSideBar/>
+          <CardSideBar />
+          <CardSideBar />
+          <CardSideBar />
+          <CardSideBar />
         </SideBar>
-        <ConteudoPrincipal/>
+        <ConteudoPrincipal>
+          {
+            artistas.map(artistas => (
+              <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+                <p>{artistas.name}</p>
+                <div className="bg-green-400 w-3/4 h-7"></div>
+                <div className="bg-green-400 w-3/4 h-7"></div>
+              </div>
+            ))
+          }
+
+
+        </ConteudoPrincipal>
       </Container>
-      </>
+    </>
   )
 }
 
